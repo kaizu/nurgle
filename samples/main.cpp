7d9da2783f71fc82484e2b05b5972376ffef6c07
Kazunari Kaizu <kwaizu@gmail.com>
2017-10-27 23:48:04 +0900

    Minor changes

--- samples/main.cpp
+++ samples/main.cpp
@@ -9,24 +9,26 @@
 int main(int argc, char* argv[])
 {
     using namespace nurgle;
-    LOG_LEVEL(Logger::L_INFO);
+    LOG_LEVEL(Logger::L_DEBUG);
 
     std::string const sep("/");
     std::string const pathto(argc > 0 ? argv[1] : ".");
 
     World w;
+    unsigned int const seed = 0;
+    w.rng.seed(seed);
 
-    for (auto elem : utils::csv<std::string, double>::read(pathto + sep + "compounds.csv"))
+    for (auto const& elem : utils::csv<std::string, double>::read(pathto + sep + "compounds.csv"))
     {
-        LOG_DEBUG("%s : %e", std::get<0>(elem), std::get<1>(elem));
+        LOG_DEBUG("%1% : %2%", std::get<0>(elem), std::get<1>(elem));
         w.pool.update(std::get<0>(elem), std::get<1>(elem));
     }
 
     std::vector<std::tuple<std::string, std::string, double>> const metabolism
         = utils::csv<std::string, std::string, double>::read(pathto + sep + "metabolism.csv");
-    for (auto elem : metabolism)
+    for (auto const& elem : metabolism)
     {
-        LOG_DEBUG("%s : %s : %e", std::get<0>(elem), std::get<1>(elem), std::get<2>(elem));
+        LOG_DEBUG("%1% : %2% : %3%", std::get<0>(elem), std::get<1>(elem), std::get<2>(elem));
     }
 
     EventScheduler<World> scheduler;
