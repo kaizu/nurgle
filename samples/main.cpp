6be4380277751e54158d3581a4edd40eb3826f2f
Kazunari Kaizu <kwaizu@gmail.com>
2017-10-27 22:27:01 +0900

    Read another output

--- samples/main.cpp
+++ samples/main.cpp
@@ -6,13 +6,23 @@ int main(int argc, char* argv[])
 {
     using namespace nurgle;
 
+    std::string const sep("/");
     std::string const pathto(argc > 0 ? argv[1] : ".");
 
-    std::string const filename("/compounds.csv");
-    std::vector<std::tuple<std::string, double>> const compounds = utils::csv<std::string, double>::read(pathto + filename);
+    std::string const filename1("compounds.csv");
+    std::vector<std::tuple<std::string, double>> const compounds
+        = utils::csv<std::string, double>::read(pathto + sep + filename1);
     for (auto elem : compounds)
     {
         std::cout << std::get<0>(elem) << " : " << std::get<1>(elem) << std::endl;
+    }
+
+    std::string const filename2("metabolism.csv");
+    std::vector<std::tuple<std::string, std::string, double>> const metabolism
+        = utils::csv<std::string, std::string, double>::read(pathto + sep + filename2);
+    for (auto elem : metabolism)
+    {
+        std::cout << std::get<0>(elem) << " : " << std::get<1>(elem) << " : " << std::get<2>(elem) << std::endl;
     }
 
     return 0;
