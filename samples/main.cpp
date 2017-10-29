3b97f04a0b1b8bb82bc8c8c6019660b1888326b4
Kazunari Kaizu <kwaizu@gmail.com>
2017-10-30 00:52:30 +0900

    Test decaying

--- samples/main.cpp
+++ samples/main.cpp
@@ -21,19 +21,22 @@ int main(int argc, char* argv[])
     w.rng.seed(seed);
 
     read_pool(pathto + sep + "compounds.csv", w.pool);
+    w.pool.update("Pimeloyl_ACPs_c", 2.0);
+    std::cout << "Pimeloyl_ACPs_c" << " = " << w.pool.get("Pimeloyl_ACPs_c") << std::endl;
 
     EventScheduler<World> scheduler;
 
     scheduler.insert(generate_fixed_interval_callback_event<World>(
-        "TimerEvent", 1.0, [&](World& w) -> std::vector<EventScheduler<World>::token_type> {
+        "TimerEvent", 10.0, [&](World& w) -> std::vector<EventScheduler<World>::token_type> {
             std::cout << "The current time is " << w.t << "." << std::endl;
+            std::cout << "Pimeloyl_ACPs_c" << " = " << w.pool.get("Pimeloyl_ACPs_c") << std::endl;
             return {};
             }));
 
     scheduler.insert(
-        generate_enzymatic_chemical_reaction_event(1.0, pathto + sep + "metabolism.csv"));
+        generate_enzymatic_chemical_reaction_event(10.0, pathto + sep + "metabolism.csv"));
 
-    scheduler.run(w, 10.0);
+    scheduler.run(w, 100.0);
 
     dump_pool(std::cout, w.pool);
 
